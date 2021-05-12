7ca09fa51a88e7e7530c422dc9e15b63a82a2e6f
Martin Tudor <martcan27@gmail.com>
2021-05-12 20:06:55 +0100

    add markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-news`,
+        path: `${__dirname}/src/markdown-news`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/pages/news.js
+++ src/pages/news.js
@@ -1,3 +1,4 @@
+import { graphql } from "gatsby"
 import React from "react"
 import styled from "styled-components"
 import Layout from "../components/layout"
@@ -5,15 +6,38 @@ import Layout from "../components/layout"
 const NewsStyles = styled.div`
   max-width: 750px;
   margin: 0 auto;
-  text-align: center;
+  h2,
+  time {
+    background: yellow;
+    color: black;
+    width: fit-content;
+    padding: 2px;
+  }
 `
 
-export default function News() {
+export default function News({ data }) {
+  const {
+    html,
+    frontmatter: { date },
+  } = data.news
   return (
     <Layout>
       <NewsStyles>
-        <h1>News Page</h1>
+        <time>{date}</time>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
       </NewsStyles>
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    news: markdownRemark {
+      html
+      frontmatter {
+        date
+        title
+      }
+    }
+  }
+`

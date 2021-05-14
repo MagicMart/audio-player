eb43d3d28bb2dd4fb730c75cf140e2bb95271a70
Martin Tudor <martcan27@gmail.com>
2021-05-14 16:21:21 +0100

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -6,26 +6,19 @@ import styled from "styled-components"
 
 const HeaderStyles = styled.header`
   background: whitesmoke;
-  /* margin-bottom: 1.45rem; */
   padding: 10px 0;
   .inner {
-    display: flex;
-    justify-content: space-evenly;
+    display: grid;
+    grid-template-columns: auto auto;
+    justify-content: center;
     align-items: center;
+    grid-column-gap: 10px;
     margin: 0 auto;
-    max-width: 750px;
-    /* padding: 1.45rem 1.0875rem; */
+    max-width: 300px;
   }
   h1 {
     margin: 0;
-  }
-  a {
-    /* display: flex;
-    align-items: center;
-    color: black;
-    letter-spacing: 1.4px;
-    text-decoration: none; */
-    font-size: 3rem;
+    font-size: 4rem;
   }
   img {
     border-radius: 25%;
@@ -35,18 +28,18 @@ const HeaderStyles = styled.header`
 const Header = ({ siteTitle }) => (
   <HeaderStyles>
     <div className="inner">
-      <h1 style={{ display: "flex", alignItems: "center" }}>
-        <Link to="/">
-          <mark>{siteTitle}</mark>
-        </Link>
-      </h1>
       <StaticImage
         src="../images/updatelogo.png"
         width={50}
         quality={100}
         formats={["AUTO", "WEBP", "AVIF"]}
         alt=""
-      />
+      />{" "}
+      <Link to="/">
+        <h1>
+          <mark>{siteTitle}</mark>
+        </h1>
+      </Link>
     </div>
   </HeaderStyles>
 )

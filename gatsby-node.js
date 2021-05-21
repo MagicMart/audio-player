77e129f282d980075c47f88d442c5733941dad3f
Martin Tudor <martcan27@gmail.com>
2021-05-21 14:56:20 +0100

    update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,7 +19,7 @@ async function fetchS3ObjectKeysAndTurnIntoNodes({
   const s3keys = await res.json()
   s3keys.forEach((s3keyRaw, i) => {
     s3key =
-      "https://d30v8sqsz26dxk.cloudfront.net/" + s3keyRaw.replace(" ", "+")
+      "https://d30v8sqsz26dxk.cloudfront.net/" + s3keyRaw.replace(/ /g, "+")
     s3key = s3key.replace("/audio", "")
     const nodeMeta = {
       id: createNodeId(`key-${s3key}`),
